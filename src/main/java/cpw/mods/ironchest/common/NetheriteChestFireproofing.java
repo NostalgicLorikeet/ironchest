e4d8c32c9cd7fbff66cd6614aa28850ad335d1e4
miles <29894500+NostalgicLorikeet@users.noreply.github.com>
2026-04-06 03:07:12 -0400

    fixing it better

--- src/main/java/cpw/mods/ironchest/common/NetheriteChestFireproofing.java
+++ src/main/java/cpw/mods/ironchest/common/NetheriteChestFireproofing.java
@@ -1,20 +1,15 @@
 package cpw.mods.ironchest.common;
 
 import cpw.mods.ironchest.common.entity.EntityItemNetheriteChest;
+import cpw.mods.ironchest.common.items.chest.ItemIronChest;
+import cpw.mods.ironchest.common.items.shulker.ItemIronShulkerBox;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.ArrayList;
-import java.util.Objects;
-
 public class NetheriteChestFireproofing {
-    public static Item IRON_CHEST_ITEM = null;
-
     //Mostly derived from https://github.com/Jboymercs/nether_backport/blob/20b89ea14bd43c244c63675b028b8a7cdb887cfa/src/main/java/com/unseen/nb/common/EntityEvents.java
     @SubscribeEvent
     public void joinWorld(EntityJoinWorldEvent event) {
@@ -24,13 +19,8 @@ public class NetheriteChestFireproofing {
                 if (!(event.getEntity() instanceof EntityItemNetheriteChest)) {
                     ItemStack item = entityItem.getItem();
 
-                    if (IRON_CHEST_ITEM == null) {
-                        IRON_CHEST_ITEM = Item.REGISTRY.getObject(new ResourceLocation("ironchest:iron_chest"));
-                    }
-
-                    //find a better way to do this
-                    if (item.getItem() == IRON_CHEST_ITEM && item.getItemDamage() == 9 ||
-                            (item.getItem().getRegistryName().toString().startsWith("ironchest:iron_shulker_box_") && item.getItemDamage() == 7)) {
+                    if (item.getItem() instanceof ItemIronChest && item.getItemDamage() == 9 ||
+                            (item.getItem() instanceof ItemIronShulkerBox && item.getItemDamage() == 7)) {
                         if (!item.isEmpty()) {
                             EntityItemNetheriteChest chest = new EntityItemNetheriteChest(world, entityItem, item);
                             entityItem.setDead();
